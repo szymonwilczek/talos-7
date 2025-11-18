9a8b6eeac5a931ca193f46ec7a859f76e77e94a2
Szymon Wilczek <szymonwilczek@outlook.com>
2025-11-18 16:20:27 +0100

    feat: implemented logging function in cdc protocol

--- firmware/src/cdc_protocol.c
+++ firmware/src/cdc_protocol.c
@@ -559,3 +559,14 @@ void cdc_protocol_task(void) {
     }
   }
 }
+
+// ==================== LOGOWANIE ====================
+void cdc_log(const char *format, ...) {
+  char buffer[256];
+  va_list args;
+  va_start(args, format);
+  vsnprintf(buffer, sizeof(buffer), format, args);
+  va_end(args);
+
+  printf("%s", buffer);
+}
