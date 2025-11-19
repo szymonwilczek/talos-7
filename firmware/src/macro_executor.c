0a1de3ec660b269f01945fcae759149dc46eb8e4
Szymon Wilczek <szymonwilczek@outlook.com>
2025-11-19 20:29:10 +0100

    feat: macro executing for text_strings
    supporting ascii and unicode characters

--- firmware/src/macro_executor.c
+++ firmware/src/macro_executor.c
@@ -1,10 +1,155 @@
+#include "cdc_protocol.h"
 #include "hardware_interface.h"
 #include "macro_config.h"
 #include "pico/stdlib.h"
-#include "cdc_protocol.h"
 #include "tusb.h"
 #include <stdio.h>
 #include <string.h>
+
+void send_key_with_modifiers(uint8_t modifiers, uint8_t keycode,
+                             uint16_t press_ms) {
+  while (!tud_hid_ready())
+    tud_task();
+  uint8_t report[6] = {keycode, 0, 0, 0, 0, 0};
+  tud_hid_keyboard_report(1, modifiers, report);
+  sleep_ms(press_ms);
+  while (!tud_hid_ready())
+    tud_task();
+  tud_hid_keyboard_report(1, modifiers, NULL); // release key, keep modifiers
+  sleep_ms(20);
+}
+
+void send_unicode(uint8_t platform, uint32_t codepoint) {
+  char hex[9];
+  sprintf(hex, "%x", codepoint);
+  cdc_log("[HID] Unicode hex (lower): %s\n", hex);
+
+  if (platform == 0) { // Linux (GTK / IBus)
+
+    // Ctrl + Shift
+    while (!tud_hid_ready())
+      tud_task();
+    tud_hid_keyboard_report(1, 0x03, NULL);
+    sleep_ms(2); // zarejestrowanie modyfikatorow
+
+    // U (trzymajac Ctrl + Shift)
+    while (!tud_hid_ready())
+      tud_task();
+
+    tud_hid_keyboard_report(1, 0x03, (uint8_t[]){24, 0, 0, 0, 0, 0});
+    sleep_ms(2);
+
+    // pusc U (nadal trzymajac Ctrl + Shift)
+    while (!tud_hid_ready())
+      tud_task();
+    tud_hid_keyboard_report(1, 0x03, NULL);
+    sleep_ms(2);
+
+    // pusc Ctrl + Shift
+    while (!tud_hid_ready())
+      tud_task();
+    tud_hid_keyboard_report(1, 0, NULL);
+    sleep_ms(15); // czekanie na reakcje gui
+
+    // hex digits
+    for (char *h = hex; *h; h++) {
+      uint8_t keycode, modifiers;
+      if (map_char_to_hid(*h, &keycode, &modifiers)) {
+        while (!tud_hid_ready())
+          tud_task();
+
+        uint8_t report[6] = {keycode, 0, 0, 0, 0, 0};
+        tud_hid_keyboard_report(1, 0, report);
+        sleep_ms(2);
+
+        while (!tud_hid_ready())
+          tud_task();
+        tud_hid_keyboard_report(1, 0, NULL); // release
+        sleep_ms(2);
+      }
+    }
+
+    // spacja
+    while (!tud_hid_ready())
+      tud_task();
+    tud_hid_keyboard_report(1, 0, (uint8_t[]){44, 0, 0, 0, 0, 0});
+    sleep_ms(2);
+
+    while (!tud_hid_ready())
+      tud_task();
+    tud_hid_keyboard_report(1, 0, NULL);
+    sleep_ms(2);
+
+  } else if (platform == 1) { // Windows: Alt + + + hex + Alt release
+    // alt down
+    while (!tud_hid_ready())
+      tud_task();
+    tud_hid_keyboard_report(1, 0x04, NULL);
+    cdc_log("[HID] Sent Alt down\n");
+    sleep_ms(100);
+    // +
+    tud_hid_keyboard_report(1, 0x04, (uint8_t[]){46, 0, 0, 0, 0, 0});
+    cdc_log("[HID] Sent +\n");
+    sleep_ms(100);
+    tud_hid_keyboard_report(1, 0x04, NULL);
+    sleep_ms(50);
+    // hex
+    for (char *h = hex; *h; h++) {
+      uint8_t keycode = 0;
+      if (*h >= '0' && *h <= '9')
+        keycode = 30 + (*h - '0');
+      else if (*h >= 'A' && *h <= 'F')
+        keycode = 4 + (*h - 'A');
+      if (keycode) {
+        while (!tud_hid_ready())
+          tud_task();
+        tud_hid_keyboard_report(1, 0x04, (uint8_t[]){keycode, 0, 0, 0, 0, 0});
+        cdc_log("[HID] Sent hex digit %c\n", *h);
+        sleep_ms(100);
+        tud_hid_keyboard_report(1, 0, NULL);
+        sleep_ms(50);
+      }
+    }
+    // alt up
+    tud_hid_keyboard_report(1, 0, NULL);
+    cdc_log("[HID] Sent Alt up\n");
+    sleep_ms(50);
+  } else if (platform == 2) { // Mac: Ctrl+Cmd+Space + hex + space
+    // Ctrl+Cmd+Space
+    while (!tud_hid_ready())
+      tud_task();
+    tud_hid_keyboard_report(1, 0x11, (uint8_t[]){44, 0, 0, 0, 0, 0});
+    cdc_log("[HID] Sent Ctrl+Cmd+Space\n");
+    sleep_ms(100);
+    tud_hid_keyboard_report(1, 0, NULL);
+    sleep_ms(50);
+    for (char *h = hex; *h; h++) {
+      uint8_t keycode, modifiers;
+      if (map_char_to_hid(*h, &keycode, &modifiers)) {
+        while (!tud_hid_ready())
+          tud_task();
+        uint8_t report[6] = {keycode, 0, 0, 0, 0, 0};
+        tud_hid_keyboard_report(1, modifiers, report);
+        cdc_log("[HID] Sent hex digit %c\n", *h);
+        sleep_ms(100);
+        tud_hid_keyboard_report(1, 0, NULL);
+        sleep_ms(50);
+      }
+    }
+    // Space
+    while (!tud_hid_ready())
+      tud_task();
+    tud_hid_keyboard_report(1, 0, (uint8_t[]){44, 0, 0, 0, 0, 0});
+    cdc_log("[HID] Sent space\n");
+    sleep_ms(100);
+    tud_hid_keyboard_report(1, 0, NULL);
+    sleep_ms(50);
+  } else {
+    cdc_log("[HID] Unicode not supported for platform %d\n", platform);
+  }
+
+  tud_hid_keyboard_report(1, 0, NULL);
+}
 
 void execute_macro(uint8_t layer, uint8_t button) {
   config_data_t *config = config_get();
@@ -43,8 +188,39 @@ void execute_macro(uint8_t layer, uint8_t button) {
   }
 
   case MACRO_TYPE_TEXT_STRING: {
-    printf("[HID] Typing text: '%s'\n", macro->macro_string);
-    // TODO
+    cdc_log("[HID] Typing text: '%s'\n", macro->macro_string);
+    uint8_t detected_os = detect_platform();
+    const char *p = macro->macro_string;
+    while (*p) {
+      uint32_t code = utf8_to_codepoint(&p);
+      if (code == 0) {
+        cdc_log("[HID] Invalid UTF-8\n");
+        continue;
+      }
+      cdc_log("[HID] Codepoint: %u (0x%X)\n", code, code);
+      if (code < 128) {
+        // ASCII
+        uint8_t keycode, modifiers;
+        if (map_char_to_hid((char)code, &keycode, &modifiers)) {
+          cdc_log("[HID] ASCII: key=%d mods=%d\n", keycode, modifiers);
+          while (!tud_hid_ready())
+            tud_task();
+          uint8_t report[6] = {keycode, 0, 0, 0, 0, 0};
+          tud_hid_keyboard_report(1, modifiers, report);
+          cdc_log("[HID] Sent ASCII press\n");
+          sleep_ms(2);
+          tud_hid_keyboard_report(1, 0, NULL);
+          cdc_log("[HID] Sent ASCII release\n");
+          sleep_ms(10);
+        }
+      } else {
+        // Unicode
+        cdc_log("[HID] Unicode for platform %d\n", detected_os);
+        send_unicode(detected_os, code);
+      }
+    }
+    tud_hid_keyboard_report(1, 0, NULL);
+    cdc_log("[EXECUTOR] Macro finished\n");
     break;
   }
 
@@ -98,4 +274,6 @@ void execute_macro(uint8_t layer, uint8_t button) {
   led_toggle(button);
   sleep_ms(100);
   led_toggle(button);
+
+  tud_hid_keyboard_report(1, 0, NULL);
 }
