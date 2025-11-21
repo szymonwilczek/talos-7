90bd2721dc78321236bfaff490aa064e19951c71
Szymon Wilczek <szymonwilczek@outlook.com>
2025-11-21 19:03:52 +0100

    feat: new test script
    quicker testing - much more efficiency

--- test.sh
+++ test.sh
@@ -1,36 +1,6 @@
 #!/bin/bash
 
-# otworz terminal uzywajac xdotool do symulacji super+enter
-xdotool key super+Return
 
-# poczekaj na uruchomienie terminala
-sleep 0.5
+echo "Dziala!";
 
-# uruchomienie firefoxa w tle
-firefox &
-
-# poczekaj az firefox sie uruchomi
-sleep 2
-
-# znajdz id okna firefoxa
-FIREFOX_WINDOW=$(xdotool search --class "firefox" | tail -1)
-
-# aktywuj okno firefoxa
-xdotool windowactivate $FIREFOX_WINDOW
-
-# poczekaj chwile na aktywacje
-sleep 0.3
-
-# przenies firefox na lewy monitor
-xdotool key super+Shift+Left
-
-# poczekaj na przesuniecie okna
-sleep 0.3
-
-# znajdz id okna terminala
-TERMINAL_WINDOW=$(xdotool search --class "gnome-terminal" | tail -1)
-
-# przywroc focus do terminala
-if [ -n "$TERMINAL_WINDOW" ]; then
-    xdotool windowactivate $TERMINAL_WINDOW
-fi
+notify-send "Skrypt dziala poprawnie!"
