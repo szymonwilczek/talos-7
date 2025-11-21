7eff28da938cf0c294c30b1f9b492739f5254ca4
Szymon Wilczek <szymonwilczek@outlook.com>
2025-11-21 18:17:37 +0100

    fix: mouse descriptor for usb

--- firmware/src/usb_descriptors.c
+++ firmware/src/usb_descriptors.c
@@ -35,7 +35,9 @@ uint8_t const *tud_descriptor_device_cb(void) {
 // HID Report Descriptor
 //--------------------------------------------------------------------+
 uint8_t const desc_hid_report[] = {
-    TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(1))};
+    TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(1)), // klawiatura
+    TUD_HID_REPORT_DESC_MOUSE(HID_REPORT_ID(2))     // myszka
+};
 
 // invoked when received GET HID REPORT DESCRIPTOR
 uint8_t const *tud_hid_descriptor_report_cb(uint8_t instance) {
