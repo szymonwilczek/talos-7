60ba983a17ec6479588f02d343319470864a55f8
Szymon Wilczek <szymonwilczek@outlook.com>
2025-11-22 00:37:57 +0100

    fix: header definition of format_sequence helper

--- firmware/include/hardware_interface.h
+++ firmware/include/hardware_interface.h
@@ -1,6 +1,7 @@
 #ifndef HARDWARE_INTERFACE_H
 #define HARDWARE_INTERFACE_H
 
+#include "macro_config.h"
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -24,5 +25,6 @@ void leds_update_for_layer(uint8_t layer);
 bool map_char_to_hid(char c, uint8_t *keycode, uint8_t *modifiers);
 uint32_t utf8_to_codepoint(const char **str);
 const char *get_key_name(uint8_t keycode);
+const char *format_sequence_short(key_step_t *sequence, uint8_t len);
 
 #endif // HARDWARE_INTERFACE_H
