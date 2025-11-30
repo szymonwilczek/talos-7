78194cb51eb1b1ac8d1f542a7d67a27ec9bd114a
Szymon Wilczek <szymonwilczek@outlook.com>
2025-11-30 22:30:16 +0100

    feat: web interface vercel push

--- web-interface/components/macro-dialog/forms/macro-form-mouse.tsx
+++ web-interface/components/macro-dialog/forms/macro-form-mouse.tsx
@@ -3,6 +3,7 @@ import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Switch } from "@/components/ui/switch";
 
+
 interface MacroFormMouseProps {
   type: 'BUTTON' | 'MOVE' | 'WHEEL';
   value: number;
